aa17930ad9091d9a3e6eaf55cffb5fa8ea57b2c3
jmcmonagleDCSD <120748576+jmcmonagleDCSD@users.noreply.github.com>
2025-01-31 10:01:33 -0700

    Player attack

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -1,26 +1,36 @@
 extends State
 class_name StateAttack
 
+
+@export var attack_sound: AudioStream 
+@export_range(1, 20, 0.5) var decelerate_speed:float = 5
+
 var attacking: bool = false
 
 @onready var idle = $"../Idle"
 @onready var walk: State = $"../Walk"
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
+@onready var attack_animation_player = $"../../Sprite2D/AttackSprite/AnimationPlayer"
+@onready var audio:AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
 
 
 func enter() -> void:
 	player.update_animation("attack")
+	attack_animation_player.play("attack_" + player.anim_direction())
+	audio.stream = attack_sound
+	audio.pitch_scale = randf_range(0.9, 1.1)
+	audio.play()
 	animation_player.animation_finished.connect(end_attack)
 	attacking = true
 	
 	
 func exit() -> void:
 	animation_player.animation_finished.disconnect(end_attack)
-	pass
+	attacking = false
 	
 
 func process(_delta: float) -> State:
-	player.velocity = Vector2.ZERO
+	player.velocity -= player.velocity * decelerate_speed * _delta
 	
 	if attacking == false:
 		if player.direction == Vector2.ZERO:
